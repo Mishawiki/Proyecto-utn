8492705044519cb356861686a270d7ce0fee783d
Mishawiki <98434004+Mishawiki@users.noreply.github.com>
2023-04-12 06:02:21 -0300

    Add files via upload
    Cambios en Paquetes

---
+++ proyecto utn/script.js
@@ -0,0 +1,4 @@
+ function abrirChat() {
+        // Reemplaza "tu-número-de-WhatsApp" con tu número de WhatsApp sin el signo "+"
+        window.open("https://wa.me/tu-número-de-WhatsApp", "_blank", "width=500,height=600");
+      }
